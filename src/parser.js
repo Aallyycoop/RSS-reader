14dc0283b52c06bbbe1363a62b79fc4716732ec6
Alexandra Dmitrieva <aallycoop@gmail.com>
2023-04-26 19:35:49 +0300

    step 4: add parser.js

---
+++ src/parser.js
@@ -0,0 +1,26 @@
+export default (data) => {
+  const parser = new DOMParser();
+  const parsedData = parser.parseFromString(data, 'application/xml');
+  // console.log(parsedData);
+
+  // парсинг фида
+  const title = parsedData.querySelector('title').textContent;
+  const description = parsedData.querySelector('description').textContent;
+  const feed = { title, description };
+  // console.log(feed);
+
+  // парсинг постов
+  const items = parsedData.querySelectorAll('item');
+  const posts = Array.from(items).map((item) => {
+    const postName = item.querySelector('title').textContent;
+    const postLink = item.querySelector('link').textContent;
+    const postDescription = item.querySelector('description').textContent;
+    return { postName, postLink, postDescription };
+  });
+  console.log(posts);
+
+  return { feed, posts };
+};
+
+// ? При возникновении ошибки парсинга приложение должно её корректно обрабатывать
+// ? и показывать сообщение в интерфейсе
